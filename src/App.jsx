5ec55aa0142c3cd5bcfc2dfc50afd09d5ee828b1
Sheepnore <a83840066@gmail.com>
2024-11-12 14:35:19 +0800

    add scores and best scores to track player's scores, add loading text while fetching api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Characters from "./components/Characters";
 import "./App.css";
 
 function App() {
   const [isGameStart, setIsGameStart] = useState(false);
+  const [scores, setScores] = useState(0);
+  const [bestScores, setBestScores] = useState(0);
+
+  useEffect(() => {
+    if (bestScores <= scores) {
+      setBestScores(scores);
+    }
+  }, [scores]);
+
   return (
     <>
       {isGameStart ? (
-        <Characters />
+        <>
+          <div className="currentScores">Scores:{scores}</div>
+          <div className="bestScores">Best Scores:{bestScores} </div>
+          <Characters setScores={setScores} scores={scores} />
+        </>
       ) : (
         <>
           <div className="startingText">
@@ -16,6 +29,10 @@ function App() {
           <button
             onClick={() => {
               setIsGameStart(true);
+              setLoading(true);
+              setTimeout(() => {
+                setLoading(false);
+              }, 2000);
             }}
           >
             Game Start!
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import "../styles/Characters.css";
 
-const Characters = () => {
+const Characters = ({ setScores }) => {
   const [charactersList, setCharactersList] = useState([]);
   const [shuffleList, setShuffleList] = useState([]);
   const [memory, setMemory] = useState([]);
   const [isShuffling, setIsShuffling] = useState(false);
+  const [isLoading, setIsLoading] = useState();
 
   const loadingGame = (arr, num) => {
     const shuffledArr = arr.sort(() => 0.5 - Math.random());
@@ -20,24 +21,39 @@ const Characters = () => {
   const handleImgClick = (character) => {
     if (!memory.includes(character)) {
       setIsShuffling(true);
+      setScores((prev) => prev + 1);
       setTimeout(() => {
         setMemory([...memory, character]);
+        setShuffleList(handleShuffling(shuffleList));
+
+        // wait for the shuffling done, otherwise the transition won't apply on all cards.
+        setTimeout(() => {
+          setIsShuffling(false);
+        }, 100);
+      }, 800);
+    } else {
+      setIsShuffling(true);
+      setScores(0);
+      setTimeout(() => {
+        setMemory([]);
         setShuffleList(handleShuffling(shuffleList));
         setTimeout(() => {
           setIsShuffling(false);
         }, 100);
-      }, 1000);
+      }, 1800);
     }
   };
   console.log(memory);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       try {
         const response = await fetch("https://genshin.jmp.blue/characters/all");
         if (!response.ok) throw new Error("Fetch data failed");
         const data = await response.json();
         setCharactersList(data);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error:", error);
       }
@@ -54,31 +70,37 @@ const Characters = () => {
   }, [charactersList]);
 
   return (
-    <div className="img-container">
-      {shuffleList.map((chrter) => {
-        return (
-          <div
-            className={isShuffling ? "card isShuffling" : "card"}
-            key={chrter.id}
-            onClick={() => {
-              handleImgClick(chrter.id);
-            }}
-          >
-            <div className="card__inner">
-              <div className="card__front">
-                <img
-                  src={`https://genshin.jmp.blue/characters/${chrter.id}/portrait`}
-                  alt={`${chrter.id}`}
-                />
+    <>
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="img-container">
+          {shuffleList.map((chrter) => {
+            return (
+              <div
+                className={isShuffling ? "card isShuffling" : "card"}
+                key={chrter.id}
+                onClick={() => {
+                  handleImgClick(chrter.id);
+                }}
+              >
+                <div className="card__inner">
+                  <div className="card__front">
+                    <img
+                      src={`https://genshin.jmp.blue/characters/${chrter.id}/portrait`}
+                      alt={`${chrter.id}`}
+                    />
+                  </div>
+                  <div className="card__back">
+                    <img src="../assets/card_back.png" />
+                  </div>
+                </div>
               </div>
-              <div className="card__back">
-                <img src="../assets/card_back.png" />
-              </div>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
